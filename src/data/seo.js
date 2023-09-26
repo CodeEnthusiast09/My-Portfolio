e40be3cc885b7c3869788749bcf5b5c267671139
O'Brien <obrienadedapo15@gmail.com>
2023-09-26 20:20:10 +0100

    commit

--- src/data/seo.js
+++ src/data/seo.js
@@ -2,36 +2,36 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a frontend developer with a strong passion for creating engaging and user-friendly web experiences. My expertise lies in crafting visually appealing and highly interactive frontends that captivate users. I am proficient in HTML, CSS, and JavaScript, and I'm well-versed in popular frontend libraries and frameworks such as React. I take pride in writing clean, maintainable code that adheres to coding standards and best practices, making it easier for teams to collaborate and maintain the frontend codebase.",
+		keywords: ["O'Brien", "O'Brien T", "O'Brien Taiwo"],
 	},
 
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a frontend developer with a strong passion for creating engaging and user-friendly web experiences. My expertise lies in crafting visually appealing and highly interactive frontends that captivate users. I am proficient in HTML, CSS, and JavaScript, and I'm well-versed in popular frontend libraries and frameworks such as React. I take pride in writing clean, maintainable code that adheres to coding standards and best practices, making it easier for teams to collaborate and maintain the frontend codebase.",
+		keywords: ["O'Brien", "O'Brien T", "O'Brien Taiwo"],
 	},
 
 	{
 		page: "articles",
 		description:
 			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: ["O'Brien", "O'Brien T", "O'Brien Taiwo"],
 	},
 
 	{
 		page: "projects",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: ["O'Brien", "O'Brien T", "O'Brien Taiwo"],
 	},
 
 	{
 		page: "contact",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: ["O'Brien", "O'Brien T", "O'Brien Taiwo"],
 	},
 ];
 
