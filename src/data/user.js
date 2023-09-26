491aa6cfc35946bb785d5e157e6eee5e2e433992
O'Brien <obrienadedapo15@gmail.com>
2023-09-26 17:54:02 +0100

    initial commit

---
+++ src/data/user.js
@@ -0,0 +1,109 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "O'Brien Taiwo",
+		email: "obrienadedapo15@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/AdedapoBrien",
+		github: "https://github.com/CodeEnthusiast09",
+		linkedin: "https://linkedin.com/in/obrien-taiwo",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Frontend Developer.",
+		description:
+			"I am a frontend developer with a strong passion for creating engaging and user-friendly web experiences. My expertise lies in crafting visually appealing and highly interactive frontends that captivate users. I am proficient in HTML, CSS, and JavaScript, and I'm well-versed in popular frontend libraries and frameworks such as React. I take pride in writing clean, maintainable code that adheres to coding standards and best practices, making it easier for teams to collaborate and maintain the frontend codebase.",
+	},
+
+	about: {
+		title: "I’m O'Brien Taiwo based in Nigeria, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback. One of my key strengths is my ability to work closely with designers and back-end developers, ensuring a seamless integration of user interfaces with backend functionality. I thrive on solving frontend challenges, optimizing performance, and enhancing user experiences. In an ever-evolving field like frontend development, I am committed to continuous learning and staying updated with the latest trends and technologies. I believe in the importance of staying adaptable and embracing new tools and methodologies to deliver cutting-edge web solutions. As a frontend developer, I am not only dedicated to creating visually stunning websites but also to providing users with intuitive and delightful interactions. I am enthusiastic about taking on new challenges and welcome opportunities that allow me to contribute to the success of projects and grow as a developer. My goal is to create web interfaces that not only meet but exceed user expectations, ultimately making the internet a more engaging and user-centric place.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Aptlearn NFT",
+			description:
+				"I recently built a mock NFT website, showcasing my skills in web development and blockchain technology integration. It's a dynamic platform that mirrors the excitement of the NFT market.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github-tutorial-for-aptlearners.vercel.app/",
+		},
+
+		{
+			title: "MovieBox",
+			description:
+				"This website offers a comprehensive platform for users to explore in-depth information about movies, including cast, release dates, and reviews, making it a one-stop destination for all things cinema.",
+			logo: "/logo512.png",
+			linkText: "View Project",
+			link: "https://movies-box-six.vercel.app/",
+		},
+
+		{
+			title: "Living A Simple Life",
+			description:
+				"'Discover the art of contentment and simplicity through our blog, where we share practical tips and insights on embracing a simpler, more fulfilling way of life.'",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://blog-orcin-theta.vercel.app/",
+		},
+
+		{
+			title: "3-Scorers",
+			description:
+				"I developed an impressive React.js application that showcases my expertise in building dynamic and responsive web interfaces, demonstrating my commitment to creating engaging user experiences with cutting-edge technology.",
+			logo: "/logo512.png",
+			linkText: "View Project",
+			link: "https://3-scorers.vercel.app/",
+		},
+
+		{
+			title: "Drag and Drop Gallery",
+			description:
+				"You will implement a fully functional and responsive image gallery that showcases a collection of images in a visually appealing way. A user should be able to use the Drag-and-Drop feature, they should be able to select and drag images, effortlessly rearranging them within the gallery.",
+			logo: "/logo512.png",
+			linkText: "View Project",
+			link: "https://dnd-gallery-kappa.vercel.app/",
+		},
+
+		{
+			title: "Travel Journal",
+			description:
+				"A static webpage built using React.I've created a captivating static travel journal webpage using React.js, showcasing my expertise in building dynamic and visually appealing user interfaces. It's a testament to my ability to craft immersive digital experiences that bring stories to life.",
+			logo: "/logo512.png",
+			linkText: "View Project",
+			link: "https://travel-journal-kohl.vercel.app/",
+		},
+
+		{
+			title: "Age Calculator",
+			description: "An age calculator app.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://age-calculator-tau-lemon.vercel.app/",
+		},
+
+		{
+			title: "Clock",
+			description: "A Clock😅.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://age-calculator-tau-lemon.vercel.app/",
+		},
+	],
+};
+
+export default INFO;
