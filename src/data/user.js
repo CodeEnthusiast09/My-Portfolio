3c363a1b6985f3f57ad99ff2b5a0bc556d84e1a6
O'Brien <obrienadedapo15@gmail.com>
2023-10-17 16:13:41 +0100

    corrected apostrophe

--- src/data/user.js
+++ src/data/user.js
@@ -22,11 +22,10 @@ const INFO = {
 	},
 
 	about: {
-		title: "I’m O'Brien Taiwo based in Nigeria, where I design the future.",
+		title: "I'm O'Brien Taiwo based in Nigeria, where I design the future.",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback. One of my key strengths is my ability to work closely with designers and back-end developers, ensuring a seamless integration of user interfaces with backend functionality. I thrive on solving frontend challenges, optimizing performance, and enhancing user experiences. In an ever-evolving field like frontend development, I am committed to continuous learning and staying updated with the latest trends and technologies. I believe in the importance of staying adaptable and embracing new tools and methodologies to deliver cutting-edge web solutions. As a frontend developer, I am not only dedicated to creating visually stunning websites but also to providing users with intuitive and delightful interactions. I am enthusiastic about taking on new challenges and welcome opportunities that allow me to contribute to the success of projects and grow as a developer. My goal is to create web interfaces that not only meet but exceed user expectations, ultimately making the internet a more engaging and user-centric place.",
 	},
-
 	articles: {
 		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
 		description:
