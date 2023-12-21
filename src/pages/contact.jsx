b3e039bdbc44f833e80b7394f1a29dd2210d201b
O'Brien <obrienadedapo15@gmail.com>
2023-12-21 09:47:23 +0100

    fixed homepage image covering header

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -33,9 +33,9 @@ const Contact = () => {
 				<NavBar active="contact" />
 				<div className="content-wrapper">
 					<div className="contact-logo-container">
-						<div className="contact-logo">
+						{/* <div className="contact-logo">
 							<Logo width={46} />
-						</div>
+						</div> */}
 					</div>
 
 					<div className="contact-container">
@@ -47,11 +47,9 @@ const Contact = () => {
 							Thank you for your interest in getting in touch with
 							me. I welcome your feedback, questions, and
 							suggestions. If you have a specific question or
-							comment, please feel free to email me directly at
-							&nbsp;{" "}
-							<a href={`mailto:${INFO.main.email}`}>
-								{INFO.main.email}
-							</a>
+							comment, please feel free to <a className="contact-link" href={`mailto:${INFO.main.email}`}>
+								{/* {INFO.main.email} */}email
+							</a> me
 							. I make an effort to respond to all messages within
 							24 hours, although it may take me longer during busy
 							periods. Alternatively, you can use the contact form
@@ -59,16 +57,14 @@ const Contact = () => {
 							required fields and I'll get back to you as soon as
 							possible. Finally, if you prefer to connect on
 							social media, you can find me on{" "}
-							<a
+							<a className="contact-link"
 								href={INFO.socials.twitter}
 								target="_blank"
 								rel="noreferrer"
 							>
-								{INFO.socials.twitter}
+								{/* {INFO.socials.twitter} */}twitter
 							</a>
-							. I post regular updates and engage with my
-							followers there, so don't hesitate to reach out.
-							Thanks again for your interest, and I look forward
+							.							Thanks again for your interest, and I look forward
 							to hearing from you!
 						</div>
 					</div>
